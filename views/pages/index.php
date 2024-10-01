ad4c4be006a98541cd63879b8f530c08b052dbe4
lucasartal56 <lucdanart@gmail.com>
2024-10-01 13:52:26 -0600

    levantar aplicación

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,6 +1,6 @@
 <div class="row mb-3">
   <div class="col text-center">
-    <h1>Sistema de control de inventario</h1>
+    <h1>CARTA DE SITUACION</h1>
   </div>
 </div>
 <div class="row justify-content-center">
